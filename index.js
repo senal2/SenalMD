f0cc826d22ca9bbad096152478dbf1dff7c44ec7
senal2 <97817147+senal2@users.noreply.github.com>
2024-11-02 21:02:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94769872326']
+const ownerNumber = ['94767707223']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
