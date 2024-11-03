6fbf1ca75e0cea7496942fa1468b3aaf126cc17f
senal2 <97817147+senal2@users.noreply.github.com>
2024-11-03 06:08:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG ⎹⎹ "https://telegra.ph/file/f2be313fe820b56b47748.png".
-ALIVE_MSG: process.env.ALIVE_IMG ⎹⎹ "Hellow Im Senal",
+SESSION_ID: process.env.SESSION_ID || Am0EETTb#Q0CLGC5zKxSaXG-3_yxDr3ILpUvY818F-o8u88mpToQ,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/f2be313fe820b56b47748.png",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I'm Senal",
 };
