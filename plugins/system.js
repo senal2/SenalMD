ea69673611c4a81a3962f413de93b9aae2a02e62
senal2 <97817147+senal2@users.noreply.github.com>
2024-11-25 12:08:27 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -3,7 +3,7 @@ const os = require("os")
 const {runtime} = require('../lib/functions')
 
 cmd({
-    pattern: "alive",
+    pattern: "system",
     alias: ["status","botinfo"],
     desc: "check up time",
     category: "main",
