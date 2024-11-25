12d3fdceaf3a93c55df23547477f5893c7444bdc
senal2 <97817147+senal2@users.noreply.github.com>
2024-11-25 09:32:15 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -2,17 +2,34 @@ const config = require('../config')
 const {cmd , commands} = require('../command')
 
 cmd({
-    pattern: "alive",
+    pattern: "senal",
     desc: "Check bot online or no.",
     category: "main",
+    react: "👋",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+
+let des = `👋 𝙷𝚎𝚕𝚕𝚘 ${pushname} 𝙸'𝚖 𝚊𝚕𝚒𝚟𝚎 𝚗𝚘𝚠
+
+*Im SENAL-MD Whatsapp Bot Create By MR SENAL 🍂✨*
+
+| *Version*: 1.0.0
+| *Memory*: 38.09MB/7930MB
+| *Owner*: SENAL MD
+
+මම Senal md whatsapp bot. මම ඔයාට උදව් කරන්නේ කෙසේ ද.
+මෙනුව ලබා ගැනීමට, .menu ලෙස ටයිප් කරන්න
+ ඔබට බොට් ගැන යමක් දැන ගැනීමට අවශ්‍ය නම්,
+.owner ලෙස ටයිප් කර ප්‍රශ්නය මා වෙත යොමු කරන්න. සුබ දිනක්
+
+*°᭄™️SENAL 𝙼𝙳*
+
+> © 𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 𝐒𝐄𝐍𝐀𝐋 𝐌𝐃`
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: des},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
 })
-
